a9f68c4a044c5ec9e727534c0b735982b8f3e6d2
piyush67 <piyushsaxena666@gmail.com>
2023-03-28 20:30:11 +0530

    updated code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+import './widgets/MainDrawer.dart';
+
+import './screens/attendence_screen.dart';
+import './screens/timetable_screen.dart';
+import './screens/update_profile_screen.dart';
+import './screens/registration_screen.dart';
+import './screens/examination_details_screen.dart';
+import './widgets/dashboard.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,6 +20,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'College Bytes',
       home: MyPage(),
+      initialRoute: '/',
+      routes: {
+        ExaminationDetailsScreen.routeName: (_) => ExaminationDetailsScreen(),
+        RegistrationScreen.routeName: (_) => RegistrationScreen(),
+        UpdateProfileScreen.routeName: (_) => UpdateProfileScreen(),
+        AttendenceScreen.routeName: (_) => AttendenceScreen(),
+        TimeTableScreen.routeName: (_) => TimeTableScreen(),
+      },
     );
   }
 }
@@ -19,9 +36,10 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: MainDrawer(),
       appBar: AppBar(title: Text('College Bytes')),
       body: Container(
-        child: Text('Work in progress...'),
+        child: DynamicEvent(),
       ),
     );
   }
